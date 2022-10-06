9479efa5fa0741b5168faca4744f8bdade693ec6
Tyler Gargula <tyler.gargula@gmail.com>
2022-10-06 12:33:40 -0500

    remove sqlalchemy

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -21,27 +20,7 @@ current_year = date.today().year
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///client_portfolio.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    subtitle = db.Column(db.String(250), nullable=True)
-    tagline = db.Column(db.String(250), nullable=True)
-    description = db.Column(db.String(250), nullable=True)
-    image = db.Column(db.String(250), nullable=True)
-    body = db.Column(db.String(500), nullable=True)
-    cms = db.Column(db.String(250), nullable=True)
-    urls_migrated = db.Column(db.Integer, nullable=True)
-    services = db.Column(db.String(250), nullable=True)
-    industry = db.Column(db.String(250), nullable=True)
-
-    def __repr__(self):
-        return f'Case Study: {self.title}'
-
-
-db.create_all()
 
 portfolio = requests.get("https://api.npoint.io/78c54b6b0ae07e92852f").json()
 
@@ -81,29 +60,6 @@ class AddForm(FlaskForm):
 def get_all_case_studies():
     return render_template('index.html', year=current_year, portfolio=portfolio_objects)
 
-
-@app.route('/add', methods=['GET', 'POST'])
-def add_work():
-    add_client_form = AddForm()
-    if add_client_form.validate_on_submit():
-        new_client = Client(
-            title=add_client_form.new_title.data,
-            subtitle=add_client_form.new_subtitle.data,
-            tagline=add_client_form.new_tagline.data,
-            description=add_client_form.new_description.data,
-            image=add_client_form.new_image.data,
-            body=add_client_form.new_body.data,
-            cms=add_client_form.new_cms.data,
-            urls_migrated=add_client_form.new_urls_migrated.data,
-            services=add_client_form.new_services.data,
-            industry=add_client_form.new_industry.data,
-
-        )
-
-        db.session.add(new_client)
-        db.session.commit()
-        print("Added client successfully")
-    return render_template('add.html', form=add_client_form)
 
 
 @app.route('/work/<int:index>-<path>')
