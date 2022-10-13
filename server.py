43f06e0ce3cc926bd4cde50d3da363a98fd88215
Tyler Gargula <tyler.gargula@gmail.com>
2022-10-13 18:06:56 -0500

    change to postgresql

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditor, CKEditorField
+from flask_sqlalchemy import SQLAlchemy
+from wtforms import StringField, SubmitField, IntegerField
+from wtforms.validators import DataRequired, Optional
 from flask_wtf import FlaskForm
 from datetime import date
 from portoflio import Portfolio
@@ -13,12 +16,19 @@ PASSWORD = os.environ["OWN_PASSWORD"]
 RECIPIENT = os.environ["RECIPIENT"]
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY_CODE']
+ckeditor = CKEditor(app)
 
-current_year = date.today().year
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///client_portfolio.db'
+# CONNECT TO DB
+if os.environ['DATABASE_URL'] == None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///client_portfolio.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+Bootstrap(app)
+current_year = date.today().year
 
 portfolio = requests.get("https://api.npoint.io/78c54b6b0ae07e92852f").json()
 
@@ -40,33 +50,72 @@ for client in portfolio:
     portfolio_objects.append(portfolio_object)
 
 
+# CONFIGURE TABLE
+class Client(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    tagline = db.Column(db.String(250), nullable=True)
+    image = db.Column(db.String(250), nullable=True)
+    body = db.Column(db.String(500), nullable=True)
+    cms = db.Column(db.String(250), nullable=True)
+    urls_migrated = db.Column(db.Integer, nullable=True)
+    services = db.Column(db.String(250), nullable=True)
+    industry = db.Column(db.String(250), nullable=True)
+    url_string = db.Column(db.String(250), nullable=True)
+
+
+# db.create_all()
+
+
+# WTForm
 class AddForm(FlaskForm):
-    new_title = StringField('Enter a title tag for your client project', validators=[DataRequired()])
-    new_subtitle = StringField('Enter a subtitle for your client project', validators=[DataRequired()])
-    new_tagline = StringField('Enter a tagline for your client project', validators=[DataRequired()])
-    new_description = StringField('Enter a description for your client project', validators=[DataRequired()])
-    new_image = StringField('Enter the image file path', validators=[DataRequired()])
-    new_body = StringField('Enter body content for your client project', validators=[DataRequired()])
-    new_cms = StringField('Enter the cms used for your client project', validators=[DataRequired()])
-    new_urls_migrated = StringField('Enter the # of URLs migrated for your client project', validators=[DataRequired()])
-    new_services = StringField('Enter the services performed for the project', validators=[DataRequired()])
-    new_industry = StringField('Enter the client industry', validators=[DataRequired()])
+    title = StringField('Enter a title tag for your client project', validators=[DataRequired()])
+    tagline = StringField('Enter a tagline for your client project', validators=[DataRequired()])
+    image = StringField('Enter the image file path', validators=[DataRequired()])
+    body = StringField('Enter body content for your client project', validators=[DataRequired()])
+    cms = StringField('Enter the cms used for your client project', validators=[DataRequired()])
+    urls_migrated = IntegerField('Enter the # of URLs migrated for your client project', validators=[Optional()])
+    services = StringField('Enter the services performed for the project', validators=[DataRequired()])
+    industry = StringField('Enter the client industry', validators=[DataRequired()])
+    url_string = StringField('Enter url string path', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+case_studies = Client.query.all()
 
 
 @app.route('/')
 def get_all_case_studies():
-    return render_template('index.html', year=current_year, portfolio=portfolio_objects)
+    print(case_studies)
+    return render_template('index.html', year=current_year, portfolio=case_studies)
 
 
-
+@app.route('/add', methods=['GET', 'POST'])
+def add_work():
+    form = AddForm()
+    if form.validate_on_submit():
+        new_client = Client(
+            title=form.title.data,
+            tagline=form.tagline.data,
+            image=form.image.data,
+            body=form.body.data,
+            cms=form.cms.data,
+            urls_migrated=form.urls_migrated.data,
+            services=form.services.data,
+            industry=form.industry.data,
+            url_string=form.url_string.data
+        )
+        db.session.add(new_client)
+        db.session.commit()
+        return redirect(url_for('get_all_case_studies'))
+    return render_template('add.html', form=form)
 
 
 @app.route('/work/<int:index>-<path>')
 def show_portfolio(index, path):
     requested_portfolio = None
 
-    for case_study in portfolio_objects:
+    for case_study in case_studies:
         if case_study.id == index:
             requested_portfolio = case_study
     return render_template("portfolio.html", year=current_year, portfolio=requested_portfolio)
@@ -74,7 +123,7 @@ def show_portfolio(index, path):
 
 @app.route('/work')
 def all_work():
-    return render_template("work.html", year=current_year, portfolio=portfolio_objects)
+    return render_template("work.html", year=current_year, portfolio=case_studies)
 
 
 @app.route('/apps')
@@ -110,4 +159,4 @@ def send_email(name, email, message):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
